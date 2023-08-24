6f332ed39c621b892023ab814eafb2235d2964d0
G D <hgd1@deloitte.com>
2023-08-24 10:45:30 +0530

    first commit

---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Dashboard() {
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Hello !!!!!</p>
+    </div>
+  );
+}
+
+export default Dashboard;
